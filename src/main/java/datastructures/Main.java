ff9de68b5a867fde7b1c5b38f3cf0c6782cd03d4
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-06 20:28:10 -0300

    feat: implementing print items

--- src/main/java/datastructures/Main.java
+++ src/main/java/datastructures/Main.java
@@ -1,7 +1,9 @@
 package datastructures;
 
+import exercises.printItems;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("WTF");
+        printItems.printItems(8);
     }
 }
---
+++ src/main/java/exercises/printItems.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class printItems {
+    public static void printItems(int n){
+        for (int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+    }
+}
